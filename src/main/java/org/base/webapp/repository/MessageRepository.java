37dfda02a953ebb05717264a4847007be3cfba04
RainbowNy <permetr35@gmail.com>
2020-06-14 14:03:33 +0300

    Added connection to database

---
+++ src/main/java/org/base/webapp/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package org.base.webapp.repository;
+
+import org.base.webapp.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
