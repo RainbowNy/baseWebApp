16099e37045d950454d24b7729cd6bc6e5d8a984
RainbowNy <permetr35@gmail.com>
2020-06-21 17:49:28 +0300

    Upload and download images

--- src/main/java/org/base/webapp/controller/BasePageController.java
+++ src/main/java/org/base/webapp/controller/BasePageController.java
@@ -4,17 +4,26 @@ import org.base.webapp.domain.Message;
 import org.base.webapp.domain.User;
 import org.base.webapp.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class BasePageController {
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -38,8 +47,23 @@ public class BasePageController {
     }
 
     @PostMapping("/main")
-    public String addMessage(@AuthenticationPrincipal User user, @RequestParam String textToSent, @RequestParam String tag, Model model){
+    public String addMessage(@AuthenticationPrincipal User user, @RequestParam String textToSent, @RequestParam String tag, @RequestParam("file")MultipartFile file, Model model) throws IOException {
         Message message = new Message(textToSent, tag, user);
+
+        if(!file.isEmpty()){
+            File uploadDir = new File(uploadPath);
+
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String fileName = uuidFile + '.' + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + fileName));
+
+            message.setFilename(fileName);
+        }
 
         messageRepository.save(message);
 
@@ -47,6 +71,6 @@ public class BasePageController {
 
        model.addAttribute("messages",messages);
 
-        return "basePage";
+        return "redirect:/main";
     }
 }
